0d352dbf671a66eaeebee210193e65f6b0c31f84
Lukasz <Lukasz.puzyniak@gmial.com>
2024-03-15 20:23:57 +0100

    update task 6

--- mdule 2 lesson 7/Program.cs
+++ mdule 2 lesson 7/Program.cs
@@ -1,6 +1,10 @@
 ﻿
 // task 1
 
+using System.Collections.Concurrent;
+using System.ComponentModel.Design;
+using System.Xml.Linq;
+
 int a = 5;
 int b = 5;
 
@@ -12,13 +16,88 @@ if (a == b)
 // task 2
 
 int c = 15;
-if ( c % 2 == 0)
+if (c % 2 == 0)
 {
     Console.WriteLine(c + " jest liczbą parzystą");
 }
 else
 {
-    Console.WriteLine( c + " jest liczbą nieparzystą");
+    Console.WriteLine(c + " jest liczbą nieparzystą");
 }
 
-//
+// task 3
+
+int d = 14;
+
+if (d > 0)
+{
+    Console.WriteLine(d + " jest liczba dodatnia");
+}
+else if (d < 0)
+{
+    Console.WriteLine(d + " jest liczba ujemna");
+}
+else
+{
+    Console.WriteLine(d + " nie jest ujemne ani dodanie jest zerem");
+}
+
+// task 4
+
+int e = 2016;
+if (e % 4 == 0)
+{
+    Console.WriteLine(e + " jest rokiem przestępnym");
+}
+else
+{
+    Console.WriteLine(e + " nie jest rokiem przestępnym");
+}
+
+// task 5
+Console.WriteLine("Please input your age:");
+byte f = byte.Parse(Console.ReadLine());
+
+if (f >= 21 && f <= 29)
+{
+    Console.WriteLine("Możesz zostać posłem i premierem");
+}
+else if (f >= 30 && f <= 34)
+{
+    Console.WriteLine("Możesz zostać posłem i premierem");
+    Console.WriteLine("Możesz zostać senatorem");
+}
+else if (f >= 35)
+{
+    Console.WriteLine("Możesz zostać posłem i premierem");
+    Console.WriteLine("Możesz zostać senatorem");
+    Console.WriteLine("Możesz zostać prezydentem");
+}
+else if (f < 21)
+{
+    Console.WriteLine("Jestes za młody");
+}
+
+// task 6
+Console.WriteLine("What's your height, please use \"cm\":");
+short g = short.Parse(Console.ReadLine());
+if ( g <= 140)
+{
+    Console.WriteLine("Jesteś krasnoludem");
+}
+else if ( g >= 141 &&  g <= 160)
+{
+    Console.WriteLine("Jesteś niski");
+}
+else if (g >= 161 && g <= 179)
+{
+    Console.WriteLine("Jesteś średniego wzrostu");
+}
+else if (g >= 180 && g <= 215)
+{
+    Console.WriteLine("Jesteś wysoki");
+}
+else if (g >= 216)
+{
+    Console.WriteLine("Jesteś gigantem");
+}
