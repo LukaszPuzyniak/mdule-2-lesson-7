51aaaa19444ade5107e894e40044bf38a6a1f483
Lukasz <Lukasz.puzyniak@gmial.com>
2024-03-15 18:39:03 +0100

    Add project files.

---
+++ mdule 2 lesson 7/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// task 1
+
+int a = 5;
+int b = 5;
+
+if (a == b)
+{
+    Console.WriteLine("5 i 5 sa równe");
+}
+
+// task 2
+
+int c = 15;
+if ( c % 2 == 0)
+{
+    Console.WriteLine(c + " jest liczbą parzystą");
+}
+else
+{
+    Console.WriteLine( c + " jest liczbą nieparzystą");
+}
+
+//
